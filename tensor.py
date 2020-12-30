5b53e134bb1734c6efccdb2dc2bb6746f26dbdfa
adelbennaceur <adelbennaceur0@gmail.com>
2020-12-30 22:11:36 +0100

    added support for subsrtaction , multiplication and sum over specific dimension

--- tensor.py
+++ tensor.py
@@ -54,6 +54,21 @@ class Tensor(object):
                     self.creators[0].backward(self.grad, self)
                     self.creators[1].backward(self.grad, self)
 
+                if self.creation_op == "neg":
+                    self.creators[0].backward(self.grad.__neg__())
+
+                if self.creation_op == "mul":
+                    new = self.grad.data * self.creators[1]
+                    self.creators[0].backward(new, self)
+                    new = self.grad.data * self.creators[0]
+                    self.creators[1].backward(new, self)
+
+                if self.creation_op == "sub":
+                    new = Tensor(self.grad.data)
+                    self.creators[0].backward(self.grad.data, self)
+                    new = Tensor(self.grad.__neg__().data)
+                    self.creators[1].backward(new, self)
+
     def __repr__(self):
         return str(self.data.__repr__())
 
@@ -67,21 +82,61 @@ class Tensor(object):
             )
         return Tensor(self.data + other.data)
 
+    def __neg__(self):
+        if self.autograd:
+            return Tensor(
+                self.data * (-1),
+                autograd=True,
+                creators=[self],
+                creation_op="neg",
+            )
+        return Tensor(self.data * (-1))
+
+    def __mul__(self, other):
+        if self.autograd and other.autograd:
+            return Tensor(
+                self.data * other.data,
+                autograd=True,
+                creators=[self, other],
+                creation_op="mul",
+            )
+
+        return Tensor(self.data * other.data)
+
+    def __sub__(self, other):
+        if self.autograd and other.autograd:
+            return Tensor(
+                self.data - other.data,
+                autograd=True,
+                creators=[self, other],
+                creation_op="sub",
+            )
+        return Tensor(self.data - other.data)
+
+    def sum(self, dim):
+        # sum over a dimension
+        if self.autograd:
+
+            return Tensor(
+                self.data.sum(dim), autograd=True, creators=[self], creation_op="sum"
+            )
+        return Tensor(self.data.sum(dim))
+
 
 if __name__ == "__main__":
     a = Tensor([1, 2, 3, 4, 5], autograd=True)
     b = Tensor([0, 5, 4, 1, 8], autograd=True)
     c = Tensor([1, 1, 1, 1, 1], autograd=True)
 
-    d = a + b
-    e = b + c
+    d = a + (-b)
+    e = (-b) + c
     f = d + e
 
     f.backward(Tensor([1, 1, 1, 1, 1]))
 
     # f.backward(Tensor([1, 1, 1, 1, 1]))
 
-    print(b.grad.data == np.array([2, 2, 2, 2, 2]))
+    print(b.grad.data == np.array([-2, -2, -2, -2, -2]))
 
     print(d.grad.data)
     print(f.creators)
