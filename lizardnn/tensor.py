203416178d3c7502e42f954a400be006d61e11b1
abennaceur <adelbennaceur0@gmail.com>
2025-05-28 16:21:57 +0200

    Refactor tensor class

--- lizardnn/tensor.py
+++ lizardnn/tensor.py
@@ -1,17 +1,39 @@
+from typing import List, Optional, Union, Tuple, Dict
 import numpy as np
+from numpy import ndarray
 
 
-class Tensor(object):
+class Tensor:
+    """Core tensor class implementing automatic differentiation.
+
+    This class wraps numpy arrays and tracks operations for automatic
+    differentiation (autograd). It builds a dynamic computational graph
+    and implements the backward pass using reverse-mode differentiation.
+    """
+
     def __init__(
-        self, data, requires_grad=False, creators=None, creation_op=None, id=None
-    ):
+        self,
+        data: Union[float, int, list, ndarray],
+        requires_grad: bool = False,
+        creators: Optional[List["Tensor"]] = None,
+        creation_op: Optional[str] = None,
+        id: Optional[int] = None,
+    ) -> None:
+        """Initialize a new tensor.
 
-        self.data = np.array(data)
+        Args:
+            data: Input data (will be converted to numpy array)
+            requires_grad: If True, gradients will be computed
+            creators: List of tensors that created this tensor
+            creation_op: The operation that created this tensor
+            id: Unique identifier for the tensor
+        """
+        self.data = np.array(data, dtype=np.float32)
         self.creation_op = creation_op
         self.creators = creators
-        self.grad = None
+        self.grad: Optional[Tensor] = None
         self.requires_grad = requires_grad
-        self.children = {}
+        self.children: Dict[int, int] = {}
 
         if id is None:
             id = np.random.randint(0, 10000)
@@ -19,110 +41,128 @@ class Tensor(object):
 
         if creators is not None:
             for c in creators:
-                # keep track of how many children a tensor has
                 if self.id not in c.children:
                     c.children[self.id] = 1
                 else:
                     c.children[self.id] += 1
 
-    def children_grads(self):
-        # check whether the tensor received the grads of all of its children
-        for id, it in self.children.items():
-            if it != 0:
-                return False
-        return True
+    def children_grads(self) -> bool:
+        """Check if all children have propagated their gradients."""
+        return all(it == 0 for it in self.children.values())
 
-    def backward(self, grad=None, grad_origin=None):
+    def backward(
+        self, grad: Optional["Tensor"] = None, grad_origin: Optional["Tensor"] = None
+    ) -> None:
+        """Compute gradients through backpropagation.
 
-        if self.requires_grad:
+        Args:
+            grad: Gradient from the next layer
+            grad_origin: Tensor that called backward on this tensor
 
-            if grad is None:
-                grad = Tensor(np.ones_like(self.data))
+        Raises:
+            Exception: If trying to backpropagate more than once through a path
+        """
+        if not self.requires_grad:
+            return
 
-            if grad_origin is not None:
-                # check if you can backpropagate then decrement the counter
-                if self.children[grad_origin.id] == 0:
-                    raise Exception("cannot backpropagate more than once")
-                else:
-                    self.children[grad_origin.id] -= 1
+        if grad is None:
+            grad = Tensor(np.ones_like(self.data))
 
-            # accumualte gradient from children
-            if self.grad is None:
-                self.grad = grad
-            else:
-                self.grad += grad
+        if grad_origin is not None:
+            if self.children[grad_origin.id] == 0:
+                raise Exception("Cannot backpropagate more than once")
+            self.children[grad_origin.id] -= 1
 
-            # grads must not have grads of their own
-            assert grad.requires_grad == False
+        # Accumulate gradients
+        if self.grad is None:
+            self.grad = grad
+        else:
+            self.grad += grad
 
-            if self.creators is not None and (
-                self.children_grads() or grad_origin is None
-            ):
-                # setattr()
-                if self.creation_op == "add":
-                    # when two tensors are added together the result has two creators
-                    self.creators[0].backward(self.grad, self)
-                    self.creators[1].backward(self.grad, self)
+        assert not grad.requires_grad, "Gradients should not require gradients"
 
-                if self.creation_op == "neg":
-                    self.creators[0].backward(self.grad.__neg__())
+        # Backpropagate to creators if all children have propagated their gradients
+        if self.creators is not None and (self.children_grads() or grad_origin is None):
+            if self.creation_op == "add":
+                self.creators[0].backward(self.grad, self)
+                self.creators[1].backward(self.grad, self)
 
-                if self.creation_op == "mul":
-                    new = self.grad * self.creators[1]
-                    self.creators[0].backward(new, self)
-                    new = self.grad * self.creators[0]
-                    self.creators[1].backward(new, self)
+            elif self.creation_op == "neg":
+                self.creators[0].backward(self.grad.__neg__(), self)
 
-                if self.creation_op == "sub":
-                    new = Tensor(self.grad.data)
-                    self.creators[0].backward(new, self)
-                    new = Tensor(self.grad.__neg__().data)
-                    self.creators[1].backward(new, self)
+            elif self.creation_op == "mul":
+                g0 = Tensor(self.grad.data * self.creators[1].data)
+                self.creators[0].backward(g0, self)
+                g1 = Tensor(self.grad.data * self.creators[0].data)
+                self.creators[1].backward(g1, self)
 
-                if self.creation_op == "transpose":
-                    self.creators[0].backward(self.grad.transpose())
+            elif self.creation_op == "sub":
+                new = Tensor(self.grad.data)
+                self.creators[0].backward(new, self)
+                new = Tensor(self.grad.__neg__().data)
+                self.creators[1].backward(new, self)
 
-                if "pow" in self.creation_op:
-                    p = int(self.creation_op.split("_")[1])
-                    self.creators[0].backward(self.grad.pow(p))
+            elif self.creation_op == "transpose":
+                self.creators[0].backward(self.grad.transpose(), self)
 
-                # matrix multiplication
-                if self.creation_op == "mm":
-                    # previous layer activation
-                    activation = self.creators[0]
-                    weights = self.creators[1]
-                    new = self.grad.mm(weights.transpose())
-                    activation.backward(new)
-                    new = self.grad.transpose().mm(activation).transpose()
-                    weights.backward(new)
+            elif "pow" in self.creation_op:
+                p = float(self.creation_op.split("_")[1])
+                self.creators[0].backward(
+                    Tensor((p * self.creators[0].data ** (p - 1)) * self.grad.data),
+                    self,
+                )
 
-                if "sum" in self.creation_op:
-                    dim = int(self.creation_op.split("_")[1])
-                    ds = self.creators[0].data.shape[dim]
-                    self.creators[0].backward(self.grad.expand(dim, ds))
+            elif self.creation_op == "mm":
+                act = self.creators[0]
+                weights = self.creators[1]
+                new = self.grad.mm(weights.transpose())
+                act.backward(new, self)
+                new = self.grad.transpose().mm(act).transpose()
+                weights.backward(new, self)
 
-                if "expand" in self.creation_op:
-                    dim = int(self.creation_op.split("_")[1])
-                    self.creators[0].backward(self.grad.sum(dim))
+            elif "sum" in self.creation_op:
+                dim = int(self.creation_op.split("_")[1])
+                ds = self.creators[0].data.shape[dim]
+                self.creators[0].backward(self.grad.expand(dim, ds))
 
-                if self.creation_op == "cross_entropy":
+            elif "expand" in self.creation_op:
+                dim = int(self.creation_op.split("_")[1])
+                self.creators[0].backward(self.grad.sum(dim))
 
-                    dx = self.softmax_out - self.target_dist
-                    self.creators[0].backward(Tensor(dx))
+            elif self.creation_op == "sigmoid":
+                self.creators[0].backward(
+                    Tensor(self.grad.data * (self.data * (1 - self.data))), self
+                )
 
-                if self.creation_op == "sigmoid":
-                    ones = Tensor(np.ones_like(self.grad.data))
-                    self.creators[0].backward(self.grad * (self * (ones - self)))
+            elif self.creation_op == "tanh":
+                self.creators[0].backward(
+                    Tensor(self.grad.data * (1 - self.data * self.data)), self
+                )
 
-                if self.creation_op == "tanh":
-                    ones = Tensor(np.ones_like(self.grad.data))
-                    self.creators[0].backward(self.grad * (ones - (self * self)))
+            elif self.creation_op == "relu":
+                self.creators[0].backward(
+                    Tensor(self.grad.data * (self.data > 0), requires_grad=False), self
+                )
 
-    def __repr__(self):
-        return str(self.data.__repr__())
+            elif self.creation_op == "leaky_relu":
+                mask = self.data > 0
+                grad_data = np.where(mask, 1.0, self.negative_slope)
+                self.creators[0].backward(
+                    Tensor(self.grad.data * grad_data, requires_grad=False), self
+                )
 
-    def __add__(self, other):
-        if self.requires_grad and other.requires_grad:
+            elif self.creation_op == "cross_entropy":
+                n = self.target_dist.shape[0]
+                dx = (self.softmax_out - self.target_dist) / n
+                self.creators[0].backward(Tensor(dx), self)
+
+    def __repr__(self) -> str:
+        return f"Tensor(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other: Union["Tensor", float, int]) -> "Tensor":
+        """Add two tensors."""
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        if self.requires_grad or other.requires_grad:
             return Tensor(
                 self.data + other.data,
                 requires_grad=True,
@@ -131,29 +171,33 @@ class Tensor(object):
             )
         return Tensor(self.data + other.data)
 
-    def __neg__(self):
+    def __neg__(self) -> "Tensor":
+        """Negate tensor."""
         if self.requires_grad:
             return Tensor(
-                self.data * (-1),
+                -self.data,
                 requires_grad=True,
                 creators=[self],
                 creation_op="neg",
             )
-        return Tensor(self.data * (-1))
+        return Tensor(-self.data)
 
-    def __mul__(self, other):
-        if self.requires_grad and other.requires_grad:
+    def __mul__(self, other: Union["Tensor", float, int]) -> "Tensor":
+        """Multiply two tensors element-wise."""
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        if self.requires_grad or other.requires_grad:
             return Tensor(
                 self.data * other.data,
                 requires_grad=True,
                 creators=[self, other],
                 creation_op="mul",
             )
-
         return Tensor(self.data * other.data)
 
-    def __sub__(self, other):
-        if self.requires_grad and other.requires_grad:
+    def __sub__(self, other: Union["Tensor", float, int]) -> "Tensor":
+        """Subtract two tensors."""
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        if self.requires_grad or other.requires_grad:
             return Tensor(
                 self.data - other.data,
                 requires_grad=True,
@@ -162,41 +206,52 @@ class Tensor(object):
             )
         return Tensor(self.data - other.data)
 
-    def pow(self, exp):
+    def __truediv__(self, other: Union["Tensor", float, int]) -> "Tensor":
+        """Divide two tensors element-wise."""
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return self * (other**-1)
+
+    def __pow__(self, exp: float) -> "Tensor":
+        """Raise tensor to a power."""
         if self.requires_grad:
             return Tensor(
-                np.power(self.data, exp),
+                self.data**exp,
                 requires_grad=True,
                 creators=[self],
-                creation_op="pow_" + str(exp),
+                creation_op=f"pow_{exp}",
             )
+        return Tensor(self.data**exp)
 
-        return Tensor(np.power(self.data, exp))
-
-    def mm(self, other):
-        if self.requires_grad:
+    def mm(self, other: "Tensor") -> "Tensor":
+        """Matrix multiplication."""
+        if self.requires_grad or other.requires_grad:
             return Tensor(
                 self.data.dot(other.data),
                 requires_grad=True,
                 creators=[self, other],
                 creation_op="mm",
             )
-
         return Tensor(self.data.dot(other.data))
 
-    def sum(self, dim):
-        # sum over a dimension
+    def sum(self, dim: int) -> "Tensor":
+        """Sum tensor along dimension."""
         if self.requires_grad:
-
             return Tensor(
                 self.data.sum(dim),
                 requires_grad=True,
                 creators=[self],
-                creation_op="sum_" + str(dim),
+                creation_op=f"sum_{dim}",
             )
         return Tensor(self.data.sum(dim))
 
-    def transpose(self):
+    def mean(self, dim: Optional[int] = None) -> "Tensor":
+        """Mean of tensor along dimension."""
+        if dim is None:
+            return Tensor(self.data.mean())
+        return self.sum(dim) / self.data.shape[dim]
+
+    def transpose(self) -> "Tensor":
+        """Transpose tensor."""
         if self.requires_grad:
             return Tensor(
                 self.data.transpose(),
@@ -204,11 +259,11 @@ class Tensor(object):
                 creators=[self],
                 creation_op="transpose",
             )
-
         return Tensor(self.data.transpose())
 
-    def expand(self, dim, copies):
-        trans = list(range(0, len(self.data.shape)))
+    def expand(self, dim: int, copies: int) -> "Tensor":
+        """Expand tensor along dimension."""
+        trans = list(range(len(self.data.shape)))
         trans.insert(dim, len(self.data.shape))
         new_shape = list(self.data.shape) + [copies]
         new_data = self.data.repeat(copies).reshape(new_shape).transpose(trans)
@@ -218,13 +273,12 @@ class Tensor(object):
                 new_data,
                 requires_grad=True,
                 creators=[self],
-                creation_op="expand_" + str(dim),
+                creation_op=f"expand_{dim}",
             )
-
         return Tensor(new_data)
 
-    def sigmoid(self):
-
+    def sigmoid(self) -> "Tensor":
+        """Apply sigmoid activation."""
         if self.requires_grad:
             return Tensor(
                 1.0 / (1.0 + np.exp(-self.data)),
@@ -232,11 +286,10 @@ class Tensor(object):
                 creators=[self],
                 creation_op="sigmoid",
             )
-
         return Tensor(1.0 / (1.0 + np.exp(-self.data)))
 
-    def tanh(self):
-
+    def tanh(self) -> "Tensor":
+        """Apply tanh activation."""
         if self.requires_grad:
             return Tensor(
                 np.tanh(self.data),
@@ -246,18 +299,48 @@ class Tensor(object):
             )
         return Tensor(np.tanh(self.data))
 
-    def relu(self):
-        raise NotImplementedError
+    def relu(self) -> "Tensor":
+        """Apply ReLU activation."""
+        if self.requires_grad:
+            return Tensor(
+                np.maximum(0, self.data),
+                requires_grad=True,
+                creators=[self],
+                creation_op="relu",
+            )
+        return Tensor(np.maximum(0, self.data))
 
-    def cross_entropy(self, tagret_idx):
+    def leaky_relu(self, negative_slope: float = 0.01) -> "Tensor":
+        """Apply Leaky ReLU activation."""
+        if self.requires_grad:
+            out = Tensor(
+                np.where(self.data > 0, self.data, self.data * negative_slope),
+                requires_grad=True,
+                creators=[self],
+                creation_op="leaky_relu",
+            )
+            out.negative_slope = negative_slope
+            return out
+        return Tensor(np.where(self.data > 0, self.data, self.data * negative_slope))
 
-        softmax_out = np.exp(self.data) / np.sum(
-            np.exp(self.data), axis=len(self.data.shape) - 1, keepdims=True
-        )
-        t = tagret_idx.data.flatten()
+    def cross_entropy(self, target_idx: "Tensor") -> "Tensor":
+        """Compute cross entropy loss.
+
+        Args:
+            target_idx: Target indices
+
+        Returns:
+            Loss tensor
+        """
+        # Compute softmax
+        exp_data = np.exp(self.data - np.max(self.data, axis=-1, keepdims=True))
+        softmax_out = exp_data / exp_data.sum(axis=-1, keepdims=True)
+
+        # Compute cross entropy
+        t = target_idx.data.flatten()
         p = softmax_out.reshape(len(t), -1)
         target_dist = np.eye(p.shape[1])[t]
-        loss = -(np.log(p) * (target_dist)).sum(1).mean()
+        loss = -(np.log(p + 1e-7) * target_dist).sum(1).mean()
 
         if self.requires_grad:
             out = Tensor(
@@ -268,7 +351,6 @@ class Tensor(object):
             )
             out.softmax_out = softmax_out
             out.target_dist = target_dist
-
             return out
 
         return Tensor(loss)
